83f11d9d31f279f61207edef03394902d7181468
lovelyAlien <54395509+lovelyAlien@users.noreply.github.com>
2021-10-14 23:04:12 +0900

    Modify CustomOAuth2UserService

--- src/main/java/com/dangsan/dotjoin/infra/oauth/CustomOAuth2UserService.java
+++ src/main/java/com/dangsan/dotjoin/infra/oauth/CustomOAuth2UserService.java
@@ -4,17 +4,28 @@ import com.dangsan.dotjoin.modules.account.model.Account;
 import com.dangsan.dotjoin.modules.account.repository.AccountRepository;
 import com.dangsan.dotjoin.modules.account.service.AccountService;
 import io.jsonwebtoken.lang.Assert;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
+@Slf4j
 @Service
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
@@ -31,96 +42,96 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     // userRequest 는 code를 받아서 accessToken을 응답 받은 객체
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        Assert.notNull(userRequest, "userRequest cannot be null");
-//		OAuth2User oAuth2User = super.loadUser(userRequest); // google의 회원 프로필 조회
+        OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = oAuth2UserService.loadUser(userRequest);
 
-        // code를 통해 구성한 정보
-        System.out.println("userRequest clientRegistration : " + userRequest.getClientRegistration());//로그인 기본 정보 제공. 어떤 OAuth로 로그인했는지 확인 가능
-        System.out.println("userRequest.getAccessToken(): "+ userRequest.getAccessToken().getTokenValue());
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        String userNameAttributeName = userRequest.getClientRegistration()
+                .getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
 
+        OAuth2Attribute oAuth2Attribute =
+                OAuth2Attribute.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
 
-        OAuth2User oauth2User= super.loadUser(userRequest);
-        System.out.println("getAttributes: "+ oauth2User.getAttributes());
-        //구글 로그인 버튼 클릭-> 구글 로그인 창-> 로그인 완료-> code를 리턴(OAuth-Client 라이브러리)->AccessToken 요청
-        //userRequest 정보->loadUser함수 호출->구글로부터 회원 프로필 받음
+        log.info("{}", oAuth2Attribute);
 
-        String provider= userRequest.getClientRegistration().getClientId(); //google
-        String providerId=oauth2User.getAttribute("sub");
-        String nickname=oauth2User.getAttribute("name");
-        String username=provider+"_"+providerId;
-        String password=bCryptPasswordEncoder.encode("GOOGLE_PASSWORD");
-        String email=oauth2User.getAttribute("email");
-        String roles="USER";
-        Account accountEntity=accountRepository.findByEmail(email);
+        var memberAttribute = oAuth2Attribute.convertToMap();
 
+        return new DefaultOAuth2User(
+                Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")),
+                memberAttribute, "email");
+    }
+}
 
-        if(accountEntity==null){
-            System.out.println("구글 로그인이 최초입니다.");
-            accountEntity = Account.builder()
-                    .email(email)
-                    .nickname(nickname)
-                    .password(password)
-                    .roles(roles)
-                    .username(username)
-                    .provider(provider)
-                    .providerId(providerId)
-                    .build();
-            accountRepository.save(accountEntity);
+@ToString
+@Builder(access = AccessLevel.PRIVATE)
+@Getter
+class OAuth2Attribute {
+    private Map<String, Object> attributes;
+    private String attributeKey;
+    private String email;
+    private String name;
+    private String picture;
+
+    static OAuth2Attribute of(String provider, String attributeKey,
+                              Map<String, Object> attributes) {
+        switch (provider) {
+            case "google":
+                return ofGoogle(attributeKey, attributes);
+            case "kakao":
+                return ofKakao("email", attributes);
+            case "naver":
+                return ofNaver("id", attributes);
+            default:
+                throw new RuntimeException();
         }
-        else{
-            System.out.println("구글 로그인을 이미 한 적 있습니다. 자동 회원가입이 되어 있습니다.");
-
-
-
-        }
-//
-//        return new PrincipalDetails(userEntity, oauth2User.getAttributes());
-        // token을 통해 응답받은 회원정보
-		System.out.println("oAuth2User : " + oauth2User);
-        return oauth2User;
-//		return processOAuth2User(userRequest, oauth2User);
     }
 
-//	private OAuth2User processOAuth2User(OAuth2UserRequest userRequest, OAuth2User oAuth2User) {
-//
-//		// Attribute를 파싱해서 공통 객체로 묶는다. 관리가 편함.
-//		OAuth2UserInfo oAuth2UserInfo = null;
-//		if (userRequest.getClientRegistration().getRegistrationId().equals("google")) {
-//			System.out.println("구글 로그인 요청~~");
-//			oAuth2UserInfo = new GoogleUserInfo(oAuth2User.getAttributes());
-//		} else if (userRequest.getClientRegistration().getRegistrationId().equals("facebook")) {
-//			System.out.println("페이스북 로그인 요청~~");
-//			oAuth2UserInfo = new FaceBookUserInfo(oAuth2User.getAttributes());
-//		} else if (userRequest.getClientRegistration().getRegistrationId().equals("naver")){
-//			System.out.println("네이버 로그인 요청~~");
-//			oAuth2UserInfo = new NaverUserInfo((Map)oAuth2User.getAttributes().get("response"));
-//		} else {
-//			System.out.println("우리는 구글과 페이스북만 지원해요 ㅎㅎ");
-//		}
-//
-//		//System.out.println("oAuth2UserInfo.getProvider() : " + oAuth2UserInfo.getProvider());
-//		//System.out.println("oAuth2UserInfo.getProviderId() : " + oAuth2UserInfo.getProviderId());
-//		Optional<User> userOptional =
-//				userRepository.findByProviderAndProviderId(oAuth2UserInfo.getProvider(), oAuth2UserInfo.getProviderId());
-//
-//		User user;
-//		if (userOptional.isPresent()) {
-//			user = userOptional.get();
-//			// user가 존재하면 update 해주기
-//			user.setEmail(oAuth2UserInfo.getEmail());
-//			userRepository.save(user);
-//		} else {
-//			// user의 패스워드가 null이기 때문에 OAuth 유저는 일반적인 로그인을 할 수 없음.
-//			user = User.builder()
-//					.username(oAuth2UserInfo.getProvider() + "_" + oAuth2UserInfo.getProviderId())
-//					.email(oAuth2UserInfo.getEmail())
-//					.role("ROLE_USER")
-//					.provider(oAuth2UserInfo.getProvider())
-//					.providerId(oAuth2UserInfo.getProviderId())
-//					.build();
-//			userRepository.save(user);
-//		}
-//
-//		return new PrincipalDetails(user, oAuth2User.getAttributes());
-//	}
+    private static OAuth2Attribute ofGoogle(String attributeKey,
+                                            Map<String, Object> attributes) {
+        return OAuth2Attribute.builder()
+                .name((String) attributes.get("name"))
+                .email((String) attributes.get("email"))
+                .picture((String)attributes.get("picture"))
+                .attributes(attributes)
+                .attributeKey(attributeKey)
+                .build();
+    }
+
+    private static OAuth2Attribute ofKakao(String attributeKey,
+                                           Map<String, Object> attributes) {
+        Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+        Map<String, Object> kakaoProfile = (Map<String, Object>) kakaoAccount.get("profile");
+
+        return OAuth2Attribute.builder()
+                .name((String) kakaoProfile.get("nickname"))
+                .email((String) kakaoAccount.get("email"))
+                .picture((String)kakaoProfile.get("profile_image_url"))
+                .attributes(kakaoAccount)
+                .attributeKey(attributeKey)
+                .build();
+    }
+
+    private static OAuth2Attribute ofNaver(String attributeKey,
+                                           Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuth2Attribute.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .attributeKey(attributeKey)
+                .build();
+    }
+
+    Map<String, Object> convertToMap() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", attributeKey);
+        map.put("key", attributeKey);
+        map.put("name", name);
+        map.put("email", email);
+        map.put("picture", picture);
+
+        return map;
+    }
 }
