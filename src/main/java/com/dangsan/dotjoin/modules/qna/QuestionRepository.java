170985c4cde06c13da9f82f10e4c57023b4f8bda
신승현 <Seunghyeon@sinhyesug.local>
2021-10-30 21:14:32 +0900

    기존에 개발된 내용과 겹치는 부분 삭제

--- src/main/java/com/dangsan/dotjoin/modules/qna/QuestionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dangsan.dotjoin.modules.qna;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuestionRepository extends JpaRepository<Question, Long>{
-
-}
--- src/main/java/com/dangsan/dotjoin/modules/tag/TagRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dangsan.dotjoin.modules.tag;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.transaction.annotation.Transactional;
-
-@Transactional(readOnly = true)
-public interface TagRepository extends JpaRepository<Tag, Long> {
-    Tag findByTitle(String title);
-}
